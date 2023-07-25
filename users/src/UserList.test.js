4ad4860e93574f145a650f51b44f5c84f91b908c
Rachel <gggggrea@gmail.com>
2023-07-25 20:45:49 +0100

    Learn how to text table contents

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -8,13 +8,15 @@ test("render one row per user", () => {
     { name: "sam", email: "sam@sam.come" },
   ];
 
-  const { container } = render(<UserList users={users} />);
-  // find all the rows in the table
+  // const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
+  // find all the rows in the table
   // way two
-  const rows = container.querySelectorAll("tbody tr");
-  //   // way one
-  //   const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  //   const rows = container.querySelectorAll("tbody tr");
+
+  // way one
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   //   // not a good way here:
   //     const rows = screen.getAllByRole("row"); // this will include header as well
@@ -25,4 +27,22 @@ test("render one row per user", () => {
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  // render the component
+  const users = [
+    { name: "jane", email: "jane@jane.come" },
+    { name: "sam", email: "sam@sam.come" },
+  ];
+
+  render(<UserList users={users} />);
+
+  screen.logTestingPlaygroundURL();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
